e9aa5b06bf14abda17aee3e7963a3c3c0e43ce10
onozaty <onozaty@gmail.com>
2019-09-28 10:50:03 +0900

    日付、真偽値の正規化処理追加(EXCELで編集した場合を考慮)

--- src/main/java/com/enjoyxstudy/redmine/issue/loader/input/FieldType.java
+++ src/main/java/com/enjoyxstudy/redmine/issue/loader/input/FieldType.java
@@ -1,10 +1,17 @@
 package com.enjoyxstudy.redmine.issue.loader.input;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
 
-@RequiredArgsConstructor
-@Getter
+import org.apache.commons.lang3.StringUtils;
+
+import lombok.Getter;
+
 public enum FieldType {
 
     ISSUE_ID("id"),
@@ -17,29 +24,79 @@ public enum FieldType {
 
     PRIORITY_ID("priority_id"),
 
+    ASSIGNED_TO_ID("assigned_to_id"),
+
+    CATEGORY_ID("category_id"),
+
+    FIXED_VERSION_ID("fixed_version_id"),
+
+    PARENT_ISSUE_ID("parent_issue_id"),
+
     SUBJECT("subject"),
 
     DESCRIPTION("description"),
 
-    CATEGORY_ID("category_id"),
+    START_DATE("start_date", FieldType::normalizeDate),
 
-    PARENT_ISSUE_ID("parent_issue_id"),
-
-    CUSTOM_FIELD("custom_field"),
-
-    ASSIGNED_TO_ID("assigned_to_id"),
-
-    FIXED_VERSION_ID("fixed_version_id"),
-
-    START_DATE("start_date"),
-
-    DUE_DATE("due_date"),
+    DUE_DATE("due_date", FieldType::normalizeDate),
 
     DONE_RATIO("done_ratio"),
 
+    IS_PRIVATE("is_private", StringUtils::lowerCase),
+
     ESTIMATED_HOURS("estimated_hours"),
 
-    IS_PRIVATE("is_private");
+    CUSTOM_FIELD("custom_field");
 
+    @Getter
     private final String fieldName;
+
+    private final Function<String, String> normalize;
+
+    private FieldType(String fieldName, Function<String, String> normalize) {
+        this.fieldName = fieldName;
+        this.normalize = normalize;
+    }
+
+    private FieldType(String fieldName) {
+        this.fieldName = fieldName;
+        this.normalize = x -> x;
+    }
+
+    public String normalize(String value) {
+        return normalize.apply(value);
+    }
+
+    private static final List<DateTimeFormatter> BEFORE_NORMALIZE_DATE_FORMATTERS = Collections.unmodifiableList(
+            Arrays.asList(
+                    DateTimeFormatter.ofPattern("yyyy/M/d"),
+                    DateTimeFormatter.ofPattern("yyyy-M-d")));
+
+    private static final DateTimeFormatter NORMALIZED_DATE_FORMATTER = DateTimeFormatter
+            .ofPattern("yyyy-MM-dd");
+
+    private static String normalizeDate(String value) {
+
+        if (StringUtils.isEmpty(value)) {
+            return value;
+        }
+
+        LocalDate date = null;
+        for (DateTimeFormatter dateFormatter : BEFORE_NORMALIZE_DATE_FORMATTERS) {
+            try {
+                date = LocalDate.parse(value, dateFormatter);
+                // 変換できたら抜ける
+                break;
+            } catch (DateTimeParseException e) {
+                // 変換できなかったら次へ
+            }
+        }
+
+        if (date == null) {
+            // 全てのフォーマットに一致しなかった場合
+            throw new IllegalArgumentException(String.format("%s is invalid date format.", value));
+        }
+
+        return NORMALIZED_DATE_FORMATTER.format(date);
+    }
 }
